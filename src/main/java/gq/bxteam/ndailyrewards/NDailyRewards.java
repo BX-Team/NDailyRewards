70f1b98cadd460dcc85839568b4cf8f414d9a106
NONPLAYT <kidzania177@gmail.com>
2023-03-26 16:46:10 +0300

    added option to enable/disable metrics

--- src/main/java/gq/bxteam/ndailyrewards/NDailyRewards.java
+++ src/main/java/gq/bxteam/ndailyrewards/NDailyRewards.java
@@ -1,5 +1,6 @@
 package gq.bxteam.ndailyrewards;
 
+import gq.bxteam.ndailyrewards.cfg.Config;
 import gq.bxteam.ndailyrewards.cfg.ConfigManager;
 import gq.bxteam.ndailyrewards.cmds.CommandManager;
 import gq.bxteam.ndailyrewards.manager.UserManager;
@@ -18,71 +19,77 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.PluginManager;
 
 public class NDailyRewards extends JavaPlugin
-    {
-        public static NDailyRewards instance;
-        private CommandManager cmd;
-        private ConfigManager cm;
-        private PluginManager pm;
-        private HookManager hm;
-        private DataManager data;
-        private UserManager um;
+{
+    public static NDailyRewards instance;
+    private CommandManager cmd;
+    private ConfigManager cm;
+    private PluginManager pm;
+    private HookManager hm;
+    private DataManager data;
+    private UserManager um;
 
-        public static NDailyRewards getInstance() {
-            return NDailyRewards.instance;
-        }
+    public static NDailyRewards getInstance() {
+        return NDailyRewards.instance;
+    }
 
-        @Override
-        public void onEnable() {
-            // Plugin startup logic
-            NDailyRewards.instance = this;
-            (this.cmd = new CommandManager(this)).setup();
-            this.getCommand("ndailyrewards").setExecutor((CommandExecutor)this.cmd);
-            this.pm = this.getServer().getPluginManager();
-            (this.hm = new HookManager(this)).setup();
-            this.load();
-            new SaveTask(this).start();
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        NDailyRewards.instance = this;
+        (this.cmd = new CommandManager(this)).setup();
+        this.getCommand("ndailyrewards").setExecutor((CommandExecutor) this.cmd);
+        this.pm = this.getServer().getPluginManager();
+        (this.hm = new HookManager(this)).setup();
+        this.load();
+        new SaveTask(this).start();
+        this.MetricsCheck();
+    }
 
-            // Initialize metrics
-            int pluginId = 13844;
-            Metrics metrics = new Metrics(this, pluginId);
-        }
+    public void onDisable() {
+        this.unload();
+    }
 
-        public void onDisable() {
-            this.unload();
-        }
+    public void load() {
+        (this.cm = new ConfigManager(this)).setup();
+        (this.data = new DataManager(this)).setup();
+        (this.um = new UserManager(this)).setup();
+    }
 
-        public void load() {
-            (this.cm = new ConfigManager(this)).setup();
-            (this.data = new DataManager(this)).setup();
-            (this.um = new UserManager(this)).setup();
-        }
-
-        public void unload() {
-            try {
-                this.getServer().getScheduler().cancelTasks((Plugin)this);
-                HandlerList.unregisterAll((Plugin)this);
-                this.um.shutdown();
-                this.data.shutdown();
-            }
-            catch (Exception e) {
-                LogUtil.send("Error while saving plugin data: " + e.getMessage(), LogType.ERROR);
-            }
-        }
-
-        public void reload() {
-            this.unload();
-            this.load();
-        }
-
-        public PluginManager getPluginManager() {
-            return this.pm;
-        }
-
-        public IDataV2 getData() {
-            return this.data.getData();
-        }
-
-        public UserManager getUserManager() {
-            return this.um;
+    public void unload() {
+        try {
+            this.getServer().getScheduler().cancelTasks((Plugin) this);
+            HandlerList.unregisterAll((Plugin) this);
+            this.um.shutdown();
+            this.data.shutdown();
+        } catch (Exception e) {
+            LogUtil.send("&cError while saving plugin data: " + e.getMessage(), LogType.ERROR);
         }
     }
+
+    public void reload() {
+        this.unload();
+        this.load();
+    }
+
+    public PluginManager getPluginManager() {
+        return this.pm;
+    }
+
+    public IDataV2 getData() {
+        return this.data.getData();
+    }
+
+    public UserManager getUserManager() {
+        return this.um;
+    }
+
+    public void MetricsCheck() {
+        if (Config.opt_metrics) {
+            // Metrics enabled - Initialize metrics
+            int pluginId = 13844;
+            Metrics metrics = new Metrics(this, pluginId);
+        } else {
+            // Metrics disabled
+        }
+    }
+}
