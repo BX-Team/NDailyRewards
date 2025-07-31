6cca49337772d94f72e33800044c12e60a442589
NONPLAY <76615486+NONPLAYT@users.noreply.github.com>
2025-08-01 02:00:15 +0300

    remove javadoc overview

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -18,7 +18,6 @@ tasks {
     javadoc {
         val options = options as StandardJavadocDocletOptions
         options.encoding = Charsets.UTF_8.name()
-        options.overview = "src/main/javadoc/overview.html"
         options.use()
         options.tags("apiNote:a:API Note:")
     }
