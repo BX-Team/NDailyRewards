e14622a8a06240cce759feff5a2cabf6857bdc23
NONPLAY <76615486+NONPLAYT@users.noreply.github.com>
2025-08-14 01:28:36 +0300

    [ci-skip] remove pluginManagement

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,3 @@
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        maven("https://repo.papermc.io/repository/maven-public/")
-    }
-}
-
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
